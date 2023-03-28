4c572245c1dffd21a0ef0cfd81056c3b856a9a14
Dominic davenport <davenport.dominic@gmail.com>
2023-03-28 18:59:24 -0500

    Done, should revisit

--- leetcode128.py
+++ leetcode128.py
@@ -2,9 +2,17 @@ nums = [0,3,7,2,5,8,4,6,0,1]
 
 class Solution:
     def longestConsecutive(self, nums):
-        nums_sorted = sorted(nums)
-        for i in range(len(nums_sorted)):
-            if
+        numSet = set(nums)
+        longest = 0
+        
+        for n in nums:
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+
     
 s = Solution()
 
