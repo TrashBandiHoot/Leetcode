df9a9e5773513edb7daff7e2fbb6e43cae5640ac
Dominic davenport <davenport.dominic@gmail.com>
2023-03-28 18:32:18 -0500

    Started

---
+++ leetcode128.py
@@ -0,0 +1,11 @@
+nums = [0,3,7,2,5,8,4,6,0,1]
+
+class Solution:
+    def longestConsecutive(self, nums):
+        nums_sorted = sorted(nums)
+        for i in range(len(nums_sorted)):
+            if
+    
+s = Solution()
+
+print(s.longestConsecutive(nums))
