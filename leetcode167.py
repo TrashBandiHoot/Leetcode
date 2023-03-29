989b8a8e646c3f3ceda14ab13d35d95dea11078d
Dominic davenport <davenport.dominic@gmail.com>
2023-03-29 18:12:31 -0500

    Completed

--- leetcode167.py
+++ leetcode167.py
@@ -1,8 +1,17 @@
-numbers = [2,7,11,15]
+numbers = [2,7,11,15] 
+target = 9
 
 class Solution:
     def twoSum(self, numbers):
-        pass
+        l, r = 0, len(numbers) - 1
+        
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r -= 1
+            elif numbers[l] + numbers[r] < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
     
 s = Solution()
 print(s.twoSum(numbers))
